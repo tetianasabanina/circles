e4d70d19a04b3ddd72099e4c13a234a8a3290a7e
Sabanina Tetiana <tatiana08st@gmail.com>
2019-09-24 17:31:47 +0300

    ReactJs Exercise Circles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Greetings(props){
+    return(
+        <div className="card">
+            <div className="member" style={{backgroundImage: `url(${props.image})`}}></div>
+            <h2>{props.name}</h2>
+            <p>from {props.city}</p>
+        
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const greet = [
+    <div>
+        <Greetings name="Petr" city="StPetersburg" image={faker.image.avatar()} />
+        <Greetings name="Mari" city="Tallinn" image={faker.image.avatar()}/>
+        <Greetings name="Onni" city="Helsinki" image={faker.image.avatar()}/>
+    </div>
+]
+ReactDOM.render(greet, document.getElementById('root'));
+
